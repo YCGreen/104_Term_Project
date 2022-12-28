714aa05a21e0e2e309f789cc22d08d8e9b620612
Yael Green <118456307+YCGreen@users.noreply.github.com>
2022-12-27 21:14:13 -0500

    edited meal_plan.html, css, and js

--- js/meal_plan.js
+++ js/meal_plan.js
@@ -1,8 +1,15 @@
-//window.alert("Welcome to this website! Please allow cookies, preferably chocolate chip.");
+//window.alert("Welcome to this website! Please allow chocolate chip cookies.");
 
 var created = new Date(2022, 12, 26);
-var today = new Date();
 
-created.toDateString();
-today.toDateString();
+document.getElementById("dateCreated").innerText=created.toDateString();
+
+
+function showConfirmation() {
+    var today = new Date();
+    var datePicker = document.getElementById("dateSelected");
+    var nextDate = datePicker.value;
+    const dateDiff = (nextDate - today) / (1000 * 60 * 60 * 24);
+    window.alert("Meal plan saved on " + today + "! Congratulations on planning for " + nextDate +".");
+}
 

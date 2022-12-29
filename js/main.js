eee45180bb0e9cbecc18d17825ca2e432b1c855b
Yael Green <118456307+YCGreen@users.noreply.github.com>
2022-12-29 16:08:41 -0500

    edited meal_plan.js, added main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-window.alert("Welcome to this website! Please allow chocolate chip cookies.");
+window.alert("Welcome to this website! Plan your meals here with our easy college-friendly recipes. Please allow chocolate chip cookies.");
 
 let dateCreated = new Date(2022, 12, 26);
 //let dateString = dateCreated.toDateString();

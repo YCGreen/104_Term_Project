deab41700fae335079196d3fefb306bed8273485
Yael Green <118456307+YCGreen@users.noreply.github.com>
2022-12-29 15:58:20 -0500

    edited meal_plan.js, added main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.alert("Welcome to this website! Please allow chocolate chip cookies.");
+
+let dateCreated = new Date(2022, 12, 26);
+//let dateString = dateCreated.toDateString();
+document.getElementById("dateCreated").innerText=dateCreated.toDateString();
--- js/meal_plan.js
+++ js/meal_plan.js
@@ -1,20 +1,13 @@
-//window.alert("Welcome to this website! Please allow chocolate chip cookies.");
-
-let dateCreated = new Date(2022, 12, 26);
-
-document.getElementById("dateCreated").innerText=dateCreated.toDateString();
-
 function showConfirmation() {
     let today = new Date();
     const newDate = getNum();
     let dateDiff = (newDate - today) / (1000 * 60 * 60 * 24);
-   // var dateD = Math.floor(dateDiff);
-    window.alert("Meal plan saved on " + today + "! Congratulations on planning for " + newDate + ", " + dateDiff + " days in advance.");
+    var dateD = Math.ceil(dateDiff);
+    window.alert("Meal plan saved on " + today + "! Congratulations on planning for " + newDate + ", " + dateD + " days in advance.");
 }
 
 function getNum() {
-  //  const datePicker = document.getElementById("dateSelected");
-    const datePicker = "2023-01-01";
+    const datePicker = document.getElementById("dateSelected");
     let nextDate = datePicker.value;
 
     var year = "";
@@ -35,7 +28,7 @@ function getNum() {
     }
     let dayInt = parseInt(day);
 
-    const newDate1 = new Date(yearInt, monthInt, dayInt);
+    const newDate1 = new Date(yearInt, monthInt-1, dayInt);
 
     return newDate1;
 

fc4f7432502e81a32519b27e605a0d17a36510b7
Yael Green <118456307+YCGreen@users.noreply.github.com>
2022-12-29 13:50:52 -0500

    edited js

--- js/meal_plan.js
+++ js/meal_plan.js
@@ -1,16 +1,42 @@
 //window.alert("Welcome to this website! Please allow chocolate chip cookies.");
 
-var dateCreated = new Date(2022, 12, 26);
+let dateCreated = new Date(2022, 12, 26);
 
 document.getElementById("dateCreated").innerText=dateCreated.toDateString();
 
 function showConfirmation() {
-    var today = new Date();
-    const datePicker = document.getElementById("dateSelected");
-    const nextDate = datePicker.value + 1;
-    const nextDate1 = new Date(nextDate);
-    var dateDiff = (nextDate1 - today) / (1000 * 60 * 60 * 24);
-    var dateD = Math.floor(dateDiff);
-    window.alert("Meal plan saved on " + today + "! Congratulations on planning for " + nextDate1 + ", " + dateD + " days in advance.");
+    let today = new Date();
+    const newDate = getNum();
+    let dateDiff = (newDate - today) / (1000 * 60 * 60 * 24);
+   // var dateD = Math.floor(dateDiff);
+    window.alert("Meal plan saved on " + today + "! Congratulations on planning for " + newDate + ", " + dateDiff + " days in advance.");
 }
 
+function getNum() {
+  //  const datePicker = document.getElementById("dateSelected");
+    const datePicker = "2023-01-01";
+    let nextDate = datePicker.value;
+
+    var year = "";
+    for(let i = 0; i < 4; i++) {
+        year += nextDate.charAt(i);
+    }
+    let yearInt = parseInt(year);
+
+    var month = "";
+    for(let i = 5; i < 7; i++) {
+        month += nextDate.charAt(i);
+    }
+    let monthInt = parseInt(month);
+
+    var day = "";
+    for(let i = 8; i < nextDate.length; i++) {
+        day += nextDate.charAt(i);
+    }
+    let dayInt = parseInt(day);
+
+    const newDate1 = new Date(yearInt, monthInt, dayInt);
+
+    return newDate1;
+
+}

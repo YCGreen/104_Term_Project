5fe18fe0131a0c7074e455aa517f941271184f70
Yael Green <118456307+YCGreen@users.noreply.github.com>
2022-12-28 19:41:24 -0500

    edited all the htmls, css, and js

--- js/meal_plan.js
+++ js/meal_plan.js
@@ -1,15 +1,16 @@
 //window.alert("Welcome to this website! Please allow chocolate chip cookies.");
 
-var created = new Date(2022, 12, 26);
+var dateCreated = new Date(2022, 12, 26);
 
-document.getElementById("dateCreated").innerText=created.toDateString();
-
+document.getElementById("dateCreated").innerText=dateCreated.toDateString();
 
 function showConfirmation() {
     var today = new Date();
-    var datePicker = document.getElementById("dateSelected");
-    var nextDate = datePicker.value;
-    const dateDiff = (nextDate - today) / (1000 * 60 * 60 * 24);
-    window.alert("Meal plan saved on " + today + "! Congratulations on planning for " + nextDate +".");
+    const datePicker = document.getElementById("dateSelected");
+    const nextDate = datePicker.value;
+    const nextDate1 = new Date(nextDate);
+    var dateDiff = (nextDate1 - today) / (1000 * 60 * 60 * 24);
+    var dateD = Math.floor(dateDiff);
+    window.alert("Meal plan saved on " + today + "! Congratulations on planning for " + nextDate1 + ", " + dateD + " days in advance.");
 }
 
